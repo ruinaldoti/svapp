a223e146f3084e26070ddc36d5d7a6e80a77bb32
ruinaldoti <ruinaldo.ti@gmail.com>
2019-11-25 14:33:49 -0300

    Projeto Criado

---
+++ com.svapp/src/test/java/com/svapp_violeta/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svapp_violeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
